0538d2818a5c62c57bedcb3d9abacc97aa0f8e64
Viktor Söderqvist <viktor.soderqvist@est.tech>
2024-10-04 12:16:47 +0200

    Fix kvstore unit tests
    Signed-off-by: Viktor Söderqvist <viktor.soderqvist@est.tech>

--- src/unit/test_kvstore.c
+++ src/unit/test_kvstore.c
@@ -2,10 +2,10 @@
 #include "test_help.h"
 
 uint64_t hashTestCallback(const void *key) {
-    return hashsetGenHashFunction((unsigned char *)key, strlen((char *)key));
+    return hashsetGenHashFunction((char *)key, strlen((char *)key));
 }
 
-int cmpTestCallback(hashset *t, void *k1, void *k2) {
+int cmpTestCallback(hashset *t, const void *k1, const void *k2) {
     UNUSED(t);
     return strcmp(k1, k2);
 }
@@ -42,17 +42,14 @@ int test_kvstoreAdd16Keys(int argc, char **argv, int flags) {
     UNUSED(flags);
 
     int i;
-    dictEntry *de;
 
     int didx = 0;
     kvstore *kvs1 = kvstoreCreate(&KvstoreHashsetTestType, 0, KVSTORE_ALLOCATE_HASHSETS_ON_DEMAND);
     kvstore *kvs2 = kvstoreCreate(&KvstoreHashsetTestType, 0, KVSTORE_ALLOCATE_HASHSETS_ON_DEMAND | KVSTORE_FREE_EMPTY_HASHSETS);
 
     for (i = 0; i < 16; i++) {
-        de = kvstoreHashsetAddRaw(kvs1, didx, stringFromInt(i), NULL);
-        TEST_ASSERT(de != NULL);
-        de = kvstoreHashsetAddRaw(kvs2, didx, stringFromInt(i), NULL);
-        TEST_ASSERT(de != NULL);
+        TEST_ASSERT(kvstoreHashsetAdd(kvs1, didx, stringFromInt(i)));
+        TEST_ASSERT(kvstoreHashsetAdd(kvs2, didx, stringFromInt(i)));
     }
     TEST_ASSERT(kvstoreHashsetSize(kvs1, didx) == 16);
     TEST_ASSERT(kvstoreSize(kvs1) == 16);
@@ -71,7 +68,6 @@ int test_kvstoreIteratorRemoveAllKeysNoDeleteEmptyHashset(int argc, char **argv,
 
     int i;
     void *key;
-    dictEntry *de;
     kvstoreIterator *kvs_it;
 
     int didx = 0;
@@ -79,20 +75,18 @@ int test_kvstoreIteratorRemoveAllKeysNoDeleteEmptyHashset(int argc, char **argv,
     kvstore *kvs1 = kvstoreCreate(&KvstoreHashsetTestType, 0, KVSTORE_ALLOCATE_HASHSETS_ON_DEMAND);
 
     for (i = 0; i < 16; i++) {
-        de = kvstoreHashsetAddRaw(kvs1, didx, stringFromInt(i), NULL);
-        TEST_ASSERT(de != NULL);
+        TEST_ASSERT(kvstoreHashsetAdd(kvs1, didx, stringFromInt(i)));
     }
 
     kvs_it = kvstoreIteratorInit(kvs1);
-    while ((de = kvstoreIteratorNext(kvs_it)) != NULL) {
+    while (kvstoreIteratorNext(kvs_it, &key)) {
         curr_slot = kvstoreIteratorGetCurrentHashsetIndex(kvs_it);
-        key = dictGetKey(de);
-        TEST_ASSERT(kvstoreHashsetDelete(kvs1, curr_slot, key) == DICT_OK);
+        TEST_ASSERT(kvstoreHashsetDelete(kvs1, curr_slot, key));
     }
     kvstoreIteratorRelease(kvs_it);
 
-    hashset *d = kvstoreGetHashset(kvs1, didx);
-    TEST_ASSERT(d != NULL);
+    hashset *s = kvstoreGetHashset(kvs1, didx);
+    TEST_ASSERT(s != NULL);
     TEST_ASSERT(kvstoreHashsetSize(kvs1, didx) == 0);
     TEST_ASSERT(kvstoreSize(kvs1) == 0);
 
@@ -107,7 +101,6 @@ int test_kvstoreIteratorRemoveAllKeysDeleteEmptyHashset(int argc, char **argv, i
 
     int i;
     void *key;
-    dictEntry *de;
     kvstoreIterator *kvs_it;
 
     int didx = 0;
@@ -115,15 +108,13 @@ int test_kvstoreIteratorRemoveAllKeysDeleteEmptyHashset(int argc, char **argv, i
     kvstore *kvs2 = kvstoreCreate(&KvstoreHashsetTestType, 0, KVSTORE_ALLOCATE_HASHSETS_ON_DEMAND | KVSTORE_FREE_EMPTY_HASHSETS);
 
     for (i = 0; i < 16; i++) {
-        de = kvstoreHashsetAddRaw(kvs2, didx, stringFromInt(i), NULL);
-        TEST_ASSERT(de != NULL);
+        TEST_ASSERT(kvstoreHashsetAdd(kvs2, didx, stringFromInt(i)));
     }
 
     kvs_it = kvstoreIteratorInit(kvs2);
-    while ((de = kvstoreIteratorNext(kvs_it)) != NULL) {
+    while (kvstoreIteratorNext(kvs_it, &key)) {
         curr_slot = kvstoreIteratorGetCurrentHashsetIndex(kvs_it);
-        key = dictGetKey(de);
-        TEST_ASSERT(kvstoreHashsetDelete(kvs2, curr_slot, key) == DICT_OK);
+        TEST_ASSERT(kvstoreHashsetDelete(kvs2, curr_slot, key));
     }
     kvstoreIteratorRelease(kvs_it);
 
@@ -131,8 +122,8 @@ int test_kvstoreIteratorRemoveAllKeysDeleteEmptyHashset(int argc, char **argv, i
     while (kvstoreIncrementallyRehash(kvs2, 1000)) {
     }
 
-    hashset *d = kvstoreGetHashset(kvs2, didx);
-    TEST_ASSERT(d == NULL);
+    hashset *s = kvstoreGetHashset(kvs2, didx);
+    TEST_ASSERT(s == NULL);
     TEST_ASSERT(kvstoreHashsetSize(kvs2, didx) == 0);
     TEST_ASSERT(kvstoreSize(kvs2) == 0);
 
@@ -147,26 +138,23 @@ int test_kvstoreHashsetIteratorRemoveAllKeysNoDeleteEmptyHashset(int argc, char 
 
     int i;
     void *key;
-    dictEntry *de;
     kvstoreHashsetIterator *kvs_di;
 
     int didx = 0;
     kvstore *kvs1 = kvstoreCreate(&KvstoreHashsetTestType, 0, KVSTORE_ALLOCATE_HASHSETS_ON_DEMAND);
 
     for (i = 0; i < 16; i++) {
-        de = kvstoreHashsetAddRaw(kvs1, didx, stringFromInt(i), NULL);
-        TEST_ASSERT(de != NULL);
+        TEST_ASSERT(kvstoreHashsetAdd(kvs1, didx, stringFromInt(i)));
     }
 
     kvs_di = kvstoreGetHashsetSafeIterator(kvs1, didx);
-    while ((de = kvstoreHashsetIteratorNext(kvs_di)) != NULL) {
-        key = dictGetKey(de);
-        TEST_ASSERT(kvstoreHashsetDelete(kvs1, didx, key) == DICT_OK);
+    while (kvstoreHashsetIteratorNext(kvs_di, &key)) {
+        TEST_ASSERT(kvstoreHashsetDelete(kvs1, didx, key));
     }
     kvstoreReleaseHashsetIterator(kvs_di);
 
-    hashset *d = kvstoreGetHashset(kvs1, didx);
-    TEST_ASSERT(d != NULL);
+    hashset *s = kvstoreGetHashset(kvs1, didx);
+    TEST_ASSERT(s != NULL);
     TEST_ASSERT(kvstoreHashsetSize(kvs1, didx) == 0);
     TEST_ASSERT(kvstoreSize(kvs1) == 0);
 
@@ -181,26 +169,23 @@ int test_kvstoreHashsetIteratorRemoveAllKeysDeleteEmptyHashset(int argc, char **
 
     int i;
     void *key;
-    dictEntry *de;
     kvstoreHashsetIterator *kvs_di;
 
     int didx = 0;
     kvstore *kvs2 = kvstoreCreate(&KvstoreHashsetTestType, 0, KVSTORE_ALLOCATE_HASHSETS_ON_DEMAND | KVSTORE_FREE_EMPTY_HASHSETS);
 
     for (i = 0; i < 16; i++) {
-        de = kvstoreHashsetAddRaw(kvs2, didx, stringFromInt(i), NULL);
-        TEST_ASSERT(de != NULL);
+        TEST_ASSERT(kvstoreHashsetAdd(kvs2, didx, stringFromInt(i)));
     }
 
     kvs_di = kvstoreGetHashsetSafeIterator(kvs2, didx);
-    while ((de = kvstoreHashsetIteratorNext(kvs_di)) != NULL) {
-        key = hashsetGetKey(de);
-        TEST_ASSERT(kvstoreHashsetDelete(kvs2, didx, key) == DICT_OK);
+    while (kvstoreHashsetIteratorNext(kvs_di, &key)) {
+        TEST_ASSERT(kvstoreHashsetDelete(kvs2, didx, key));
     }
     kvstoreReleaseHashsetIterator(kvs_di);
 
-    hashset *d = kvstoreGetHashset(kvs2, didx);
-    TEST_ASSERT(d == NULL);
+    hashset *s = kvstoreGetHashset(kvs2, didx);
+    TEST_ASSERT(s == NULL);
     TEST_ASSERT(kvstoreHashsetSize(kvs2, didx) == 0);
     TEST_ASSERT(kvstoreSize(kvs2) == 0);
 
